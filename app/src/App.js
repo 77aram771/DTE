a6442d70daa2e14cbe5768ebe1c567ff2d595828
77aram771 <77aram39@gmail.com>
2019-06-27 00:10:17 +0400

    Add Fomantic-UI Leab

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'fomantic-ui-css/semantic.css';
+import {Container, Header} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+
+            <Container>
+                <Header>
+                    Fomantic-UI & Semantic-UI-React
+                </Header>
+            </Container>
+
+        );
+    }
 }
-
 export default App;
